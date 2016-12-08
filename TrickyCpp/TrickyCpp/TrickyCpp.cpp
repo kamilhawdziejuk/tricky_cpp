2cc0e824b256bad81bfafa5c9ef11783ebf8a544
Kamil Hawdziejuk <kamil.hawdziejuk@pl.abb.com>
2016-12-08 16:48:03 +0100

    PrivateInheritance

--- TrickyCpp/TrickyCpp/TrickyCpp.cpp
+++ TrickyCpp/TrickyCpp/TrickyCpp.cpp
@@ -2,9 +2,12 @@
 //
 
 #include "stdafx.h"
+#include "WhatIf\PrivateInheritance.cpp"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	PrivateInheritance *p = new PrivateInheritance();
+	p->Test();
 	return 0;
 }
 
---
+++ TrickyCpp/TrickyCpp/WhatIf/PrivateInheritance.cpp
@@ -0,0 +1,33 @@
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+class PrivateInheritance
+{
+	class A {
+	public:
+		virtual void f(const A& a)
+		{
+			cout << "A" << endl;
+		}
+	};
+
+	class B : private A {
+	public:
+		virtual void f(const A& a)
+		{
+			cout << "B" << endl;
+		}
+	};
+
+public:
+	void Test()
+	{
+		B a1;
+		B a2;
+
+		a1.f(a2);
+	}
+
+};
