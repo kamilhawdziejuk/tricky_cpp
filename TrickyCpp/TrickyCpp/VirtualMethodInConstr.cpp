3e2d2c3619ddd5a9be2aaa516b0de7e4e56ad9e8
Kamil Hawdziejuk <kamil.hawdziejuk@pl.abb.com>
2016-12-08 12:40:16 +0100

    VirtualMethodInConstr.cpp

---
+++ TrickyCpp/TrickyCpp/VirtualMethodInConstr.cpp
@@ -0,0 +1,38 @@
+#include "stdafx.h"
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+class VirtualMethodInConstr
+{
+	class A {
+	public:
+
+		A()
+		{
+			f();
+		}
+
+		virtual void f()
+		{
+			cout << "A" << endl;
+		}
+	};
+
+	class B : public A {
+	public:
+		virtual void f()
+		{
+			cout << "B" << endl;
+		}
+	};
+
+public:
+	void Test()
+	{
+		shared_ptr<A> a(new B);
+		a->f(); //will print "A" only
+	}
+
+};
