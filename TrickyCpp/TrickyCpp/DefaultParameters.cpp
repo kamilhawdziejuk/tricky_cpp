e08af384fae23046fc5f7c50d7fb3c6002368ae9
Kamil Hawdziejuk <kamil.hawdziejuk@pl.abb.com>
2016-12-08 12:34:57 +0100

    Initial commit - DefaultParameters

---
+++ TrickyCpp/TrickyCpp/DefaultParameters.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include <memory>
+#include <iostream>
+
+using namespace std;
+
+class DefaultParameters
+{
+	class A {
+	public:
+
+		virtual void f(int nr = 1)
+		{
+			cout << nr << endl;
+		}
+	};
+
+	class B : public A {
+	public:
+		virtual void f(int nr = 2)
+		{
+			cout << nr << endl;
+		}
+	};
+
+public:
+	void Test()
+	{
+		shared_ptr<A> a(new B);		
+		a->f(); //will fire B::f() but with nr==1
+	}
+
+};
